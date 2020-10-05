926b1ad57eef9387e62d92bb3153a3c6086c1da6
user <user@192.168.0.6>
2020-10-05 19:06:02 +0530

    HealthCare

---
+++ hcms-diagnostic_center-management-system/src/main/java/com/capg/hcms/diagnostic_center_management/HcmsDiagnosticCenterManagementSystemApplication.java
@@ -0,0 +1,21 @@
+package com.capg.hcms.diagnostic_center_management;
+
+import java.util.Random;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+
+public class HcmsDiagnosticCenterManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcmsDiagnosticCenterManagementSystemApplication.class, args);
+	}
+@Bean
+public Random generateRandom()
+{
+return new Random();	
+}
+}
